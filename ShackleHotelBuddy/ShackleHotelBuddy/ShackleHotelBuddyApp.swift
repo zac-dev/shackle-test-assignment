d33994458997fdd61a4e77f4e426ce833213cfa5
Slava Khlichkin <vyacheslav.khlichkin@gmail.com>
2023-07-21 13:56:40 +0200

    Squashed commit of the following:
    commit 673409ce645fe40b54489071ec19f29c2f86c9bf
    Author: Slava Khlichkin <vyacheslav.khlichkin@gmail.com>
    Date:   Fri Jul 21 13:53:00 2023 +0200
    
        resources added
    
    commit 752c714cc23de20ade81c72f9dbf9e7f08143f4b
    Author: Slava Khlichkin <vyacheslav.khlichkin@gmail.com>
    Date:   Fri Jul 21 11:47:12 2023 +0200
    
        Initial setup + images

---
+++ ShackleHotelBuddy/ShackleHotelBuddy/ShackleHotelBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShackleHotelBuddyApp.swift
+//  ShackleHotelBuddy
+//
+//  Created by Shackle on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShackleHotelBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
